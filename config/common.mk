b4167e9018db403c46b1d21716ceca9192f10457
rc420head <dennislebhardt79@gmail.com>
2018-12-04 21:17:12 -0600

    android-9.0.0_r21

--- config/common.mk
+++ config/common.mk
@@ -137,7 +137,7 @@ $(call inherit-product, vendor/assets/common.mk)
 # aquarios first version.
 PRODUCT_VERSION_MAJOR = 9
 PRODUCT_VERSION_MINOR = 0
-PRODUCT_VERSION_MAINTENANCE = 0_r16
+PRODUCT_VERSION_MAINTENANCE = 0_r21
 AQUARIOS_POSTFIX := -$(shell date +"%Y%m%d-%H%M")
 ifdef AQUARIOS_BUILD_EXTRA
     AQUARIOS_POSTFIX := -$(AQUARIOS_BUILD_EXTRA)
