997ff1ade213656ece958233b987d329aa15d947
ElDainosor <eldainosor@gmail.com>
2018-12-23 14:02:11 -0500

    [2/3] vendor: Update weather code to use the new WeatherClient
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -72,7 +72,8 @@ PRODUCT_COPY_FILES += \
 
 # Weather
 PRODUCT_COPY_FILES += \
-    vendor/aquarios/prebuilt/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/aquarios/prebuilt/etc/permissions/com.android.providers.weather.xml:system/etc/permissions/com.android.providers.weather.xml \
+    vendor/aquarios/prebuilt/etc/default-permissions/com.android.providers.weather.xml:system/etc/default-permissions/com.android.providers.weather.xml
 
 # Required packages
 PRODUCT_PACKAGES += \
