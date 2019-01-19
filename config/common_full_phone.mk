0325d2759045413f18a9c4298a92678fcdf26649
rc420head <dennislebhardt79@gmail.com>
2019-01-18 23:58:14 -0600

    include caf_fw.mk in all full_phone configs

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,7 @@
 # Inherit common stuff
 $(call inherit-product, vendor/aquarios/config/common.mk)
 $(call inherit-product, vendor/aquarios/config/common_apn.mk)
+$(call inherit-product, vendor/aquarios/config/caf_fw.mk)
 
 # Telephony 
 PRODUCT_PACKAGES += \
