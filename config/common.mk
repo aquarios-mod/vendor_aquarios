aed203c00678542c2d14469236e69e1f06659693
rc420head <dennislebhardt79@gmail.com>
2019-04-05 16:49:40 -0500

    android-9.0.0_r35
    Change-Id: Ib29b36747790ee2f5bcefd44093dd855a29f31b1

--- config/common.mk
+++ config/common.mk
@@ -182,7 +182,7 @@ $(call inherit-product, vendor/assets/common.mk)
 # aquarios first version.
 PRODUCT_VERSION_MAJOR = 9
 PRODUCT_VERSION_MINOR = 0
-PRODUCT_VERSION_MAINTENANCE = 0_r34
+PRODUCT_VERSION_MAINTENANCE = 0_r35
 AQUARIOS_POSTFIX := -$(shell date +"%Y%m%d-%H%M")
 ifdef AQUARIOS_BUILD_EXTRA
     AQUARIOS_POSTFIX := -$(AQUARIOS_BUILD_EXTRA)
